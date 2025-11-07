f026b057abd7f50c8cdeb74b189ca39f39355cc4
UyenUyen2703 <uyen.mt40156@sinhvien.hoasen.edu.vn>
2025-11-07 11:47:48 +0700

    'update-loginform'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { HomePage } from './home-page/home-page';
 import { Routes } from '@angular/router';
 import { ProductList } from './product-list/product-list';
 import { DetailProduct } from './product-list/detail-product/detail-product';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
 export const routes: Routes = [
 
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'register',
+    component: Register
+  },
   {
     path: '',
     component: HomePage
